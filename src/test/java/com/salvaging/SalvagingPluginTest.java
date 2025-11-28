89de2e751079dcac0be75225455036b6877291eb
Anetellie <83808758+Anetellie@users.noreply.github.com>
2025-11-28 03:14:54 +0100

    salvaging

--- src/test/java/com/salvaging/SalvagingPluginTest.java
+++ src/test/java/com/salvaging/SalvagingPluginTest.java
@@ -1,14 +1,17 @@
-package com.example;
+package com.salvaging;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 import net.runelite.client.plugins.salvaging.SalvagingPlugin;
 
-public class ExamplePluginTest
+public class SalvagingPluginTest
 {
     public static void main(String[] args) throws Exception
     {
+        // Load your Salvaging plugin into the external plugin manager
         ExternalPluginManager.loadBuiltin(SalvagingPlugin.class);
+
+        // Start RuneLite
         RuneLite.main(args);
     }
 }
